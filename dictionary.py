112a42109e8b593903cd248ca22206d053276f3b
WycliffeMuchumi <muchumimaina94@gmail.com>
2021-07-20 12:40:35 -0700

    0 to hero

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Wycliffe Muchumi",
+    "age": 26,
+    "occupation": "Software Engineer",
+    "height": "6Ft"
+}
+print("My name is %s" %(student["name"]))
+
+# Adding a new key and its value to a dictionary
+# We just assign a value to a dictionary key
+student["favorite_color"] = "blue"
+print(student)
---
+++ list.py
@@ -0,0 +1,23 @@
+my_list = [1,2,3,4,5]
+print(my_list[0]) #1
+print(my_list[1]) #2
+print(my_list[4]) #5
+
+relatives = [
+    "Anne",
+    "Michael",
+    "Miriam",
+    "Joyce",
+    "Mary",
+    "Abigail"
+]
+print(relatives[3]) #Joyce
+
+# Adding an element to a list using the append method
+# Using our defined relatives above
+relatives.append("Maurice")
+print(relatives)
+
+# Looping through lists
+for name in relatives:
+    print(name)
