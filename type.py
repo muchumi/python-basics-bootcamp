19c46f225c207fffae76b7abcfba4ff6b5efaf8f
WycliffeMuchumi <muchumimaina94@gmail.com>
2021-07-23 08:00:57 -0700

    pythoning

---
+++ type.py
@@ -0,0 +1,19 @@
+# List
+chores = ['Brush teeth', 'Bath', 'Prepare Breakfast', 'Pertake Breakfast']
+
+print(type(chores)) # list
+
+# Dictionary
+student = {
+    "name": "Maurice Maina",
+    "age": "7",
+    "yearOfBirth": "2034"
+}
+
+print(type(student)) # dict
+
+# Arithmetic Operations
+numOne = 23
+numTwo = 54
+numThree = numOne + numTwo
+print(numThree, type(numThree)) # 77 <class 'int'>
---
+++ typeConversions.py
@@ -0,0 +1,12 @@
+# Using the float builtin function to convert an integer to a float
+numOne = 23
+numTwo = 54
+numThree = numOne + numTwo
+print(float(numThree))
+
+# String Conversions
+# Using int() function to convert string into an integer
+string = '123'
+secondString = int(string)
+print(secondString, type(secondString)) # 123 <class 'int'>
+
