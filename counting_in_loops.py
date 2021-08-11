00babd8e073b7a78e730af4de3d37c407576a4c5
WycliffeMuchumi <muchumimaina94@gmail.com>
2021-08-10 21:19:58 -0700

    initial commit

---
+++ counting_in_loops.py
@@ -0,0 +1,6 @@
+# Increasing the counter with 1 after every loop
+counter = 0
+myArray = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+for num in myArray:
+    counter += 1
+    print("Counter:", counter)
---
+++ filtering_in_loop.py
@@ -0,0 +1,7 @@
+# Printing all numbers that are greater than 12
+myArray = [2, 34, 8, 16, 32, 4, 1]
+for num in myArray:
+    if num > 12:
+        print(num)
+
+
