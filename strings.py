14a27a8a8bb217e3f88bebc2c98152abf4d433e7
WycliffeMuchumi <muchumimaina94@gmail.com>
2021-10-23 01:04:27 -0700

    strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+# concatenate strings/joining two strings to form statements
+stringOne = "Hello"
+stringTwo = "World"
+print(stringOne + ' ' + stringTwo)
+
+# accessing individual letters in words using the index([]) function
+myString = 'banana'
+letter = myString[3]
+print(letter)
+
+# expressions
+letterX = 3
+letterOne = myString[letterX - 1]
+print(letterOne)
+
+# Checking the length of myString
+checking_length = len(myString)
+print(checking_length)
