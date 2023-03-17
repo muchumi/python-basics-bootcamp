6ff65f681d3cc267c29de44663221190d873f850
WycliffeMuchumi <muchumimaina94@gmail.com>
2023-03-17 07:40:47 +0300

    Formatting output using str.format() method.

---
+++ string_format.py
@@ -0,0 +1,5 @@
+yes_votes = 5550000
+no_votes = 4450000
+total_votes = yes_votes + no_votes
+results_percentage = (yes_votes / total_votes) * 100
+print("The winners are {} with a {} percentage.".format(yes_votes, results_percentage))
