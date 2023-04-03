5a62c198e6a34f166fc96143a8490c4579270fe4
WycliffeMuchumi <muchumimaina94@gmail.com>
2023-04-03 05:35:21 +0300

    Handling Exceptions

--- try_and_except.py
+++ try_and_except.py
@@ -3,3 +3,12 @@ try:
         read_data = file.read()
 except:
     print('Could not open logs.txt file')
+
+
+#Example 2
+while True:
+    try:
+        number = int(input("Enter an integer"))
+        print(number, "is an integer")
+    except ValueError:
+        print("is not a valid integer")
