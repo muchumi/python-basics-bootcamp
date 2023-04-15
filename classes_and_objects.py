1873ac4f2b1c806455e1fd8ba577b33d6338e59b
WycliffeMuchumi <muchumimaina94@gmail.com>
2023-04-15 21:04:09 +0300

    Usage of Instance Variables

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -1,9 +1,9 @@
+# Example One
 class Dog:
     kind = 'canine'     # class variable shared by all instances
 
     def __init__(self, name):
         self.name = name   # instance variable unique to each instance
-
 
 breedOne = Dog('Rex') 
 breedTwo = Dog('Alpha')       
@@ -16,4 +16,27 @@ breedTwo = Dog('Alpha')
 'Rex'
 >>> breedTwo.name       # unique to breedTwo
 'Alpha'
+"""
+
+# Example Two
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        self.tricks = [] # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+breedOne = Dog('Rex')
+breedTwo = Dog('Alpha')
+
+"""
+
+>>> breedOne.add_trick('roll over')
+>>> breedTwo.add_trick('play dead')
+>>> breedOne.tricks
+['roll over']
+>>> breedTwo.tricks
+['play dead']
+
 """
