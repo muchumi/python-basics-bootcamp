6c7167f045405416dd82e6c87bb126e16173044f
WycliffeMuchumi <muchumimaina94@gmail.com>
2023-04-18 05:33:18 +0300

    Methods calling other methods by using method attributes of the self argument

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -56,3 +56,17 @@ personalityTwo = Entertainer()
 personalityTwo.movies = 'Horror'
 print(personalityTwo.profession, personalityTwo.movies)
 
+"""
+    Methods may call other methods by using method attributes of the self argument.
+"""
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
+
+    def addTwice(self, x):
+        self.add(x)
+        self.add(x)
+
