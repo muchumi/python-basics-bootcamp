134fa3f14b71b92a2b8ccbd529128155c3d42161
WycliffeMuchumi <muchumimaina94@gmail.com>
2023-04-25 05:22:37 +0300

    Iterator for looping over a sequence backwards

--- iterators.py
+++ iterators.py
@@ -10,4 +10,23 @@ for key in {'one': 1, 'two': 2}:
 for char in "123":
     print(char)
 
+"""
+    Iterator for looping over a sequence backwards.
+"""
+class Reverse:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+    
+    def __iter__(self):
+        return self
 
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse('Wycliffe')
+for item in rev:
+    print(item)
